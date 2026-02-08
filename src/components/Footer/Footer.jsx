489462f541e734995d9a9a9be7f13a849d6c033e
JASMINE CURTIS <JASMINERUTHCURTIS@GMAIL.COM>
2026-02-08 13:08:06 -0800

    footer configuration and styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,23 +1,12 @@
+import "./Footer.css";
+
 function Footer() {}
 
-// return (
-//   <header className="header">
-//     <img className="header__logo" src={logo} />
-//     <p className="header__date-and-location">
-//       {currentDate}, {weatherData.city}
-//     </p>
-//     <button
-//       onClick={handleAddClick}
-//       type="button"
-//       className="header__add-clothes-btn"
-//     >
-//       + Add clothes
-//     </button>
-//     <div className="header__user-container">
-//       <p className="header__username">Terrence Tegegne</p>
-//       <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
-//     </div>
-//   </header>
-// );
+return (
+  <div className="footer">
+    <p className="footer__name">Developed by Jasmine Curtis</p>
+    <p className="footer__date"> 2026 </p>
+  </div>
+);
 
 export default Footer;
