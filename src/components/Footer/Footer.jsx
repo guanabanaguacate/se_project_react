1e832ef39a052191b149fa5a00b7bbb494ff7772
JASMINE CURTIS <JASMINERUTHCURTIS@GMAIL.COM>
2026-02-09 08:42:28 -0800

    footer configuration

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,12 @@
 import "./Footer.css";
 
-<div className="footer">
-  <p className="footer__name">Developed by Jasmine Curtis</p>
-  <p className="footer__date"> 2026 </p>
-</div>;
+function Footer() {
+  return (
+    <div className="footer">
+      <p className="footer__name">Developed by Jasmine Curtis</p>
+      <p className="footer__date"> 2026 </p>
+    </div>
+  );
+}
 
 export default Footer;
